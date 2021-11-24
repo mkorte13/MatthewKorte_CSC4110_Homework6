bf2e137192ab1417724d2fee439b14bd98770070
mkorte13 <fy9633@wayne.edu>
2021-11-23 22:19:10 -0500

    Homework 6
    Not all code for our project is here, but everything needed for testing GamesList.js is in this repo

---
+++ functions.js
@@ -0,0 +1,11 @@
+const functions = {
+    add: (num1, num2) => num1 + num2
+}
+
+module.exports = functions;
+
+//const functions = require('./functions');
+
+//test('adds 2 + 2 to equal 4', () => {
+//    expect(functions.add(2, 2)).toBe(4);
+//});
